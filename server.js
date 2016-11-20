1718559d80dd98d19c30ffda3be6211d2ab1919c
Aleyamma <julieelsathomas@gmail.com>
2016-11-20 19:34:50 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ Pool.query("Select * from article where title ='"+ req.params.articleName +"'",f
         res.status(500).send(err.Tostring());
     }else{
         if (result.rows.length===0){
-            res.statss(404).send('Árticle not found');
+            res.status(404).send('Árticle not found');
         }else{
             var articleData=result.rows[0];
             res.send(createTemplate(articleData));
